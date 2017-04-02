88fea9b569a840961c6c92c18e16dc4c7673b0e9
hing8596 <zyxsin2011@gmail.com>
2017-04-02 20:50:30 +0800

    增加了一些注释和说明

--- getallguids.py
+++ getallguids.py
@@ -1,25 +1,23 @@
 ﻿# -*- coding: utf-8 -*-
 
-
+#因为估计参考的多余直接下下来用的 ，所以这里有很多具体到冗余的注释
 import sys
 reload(sys) 
 sys.setdefaultencoding( "utf-8" ) #避免中文乱码
 
 from evernote.api.client import EvernoteClient
-devToken = "[Enter you developer token]"  
-client = EvernoteClient(token = devToken,sandbox=False)
+devToken = "[Enter you developer token]"  #这里源码不要忘记改
+client = EvernoteClient(token = devToken,sandbox=False)#这里沙盒一定要设置为False，否则授权直接失败
 note_store = client.get_note_store()
 user_store = client.get_user_store()
-from evernote.edam.notestore import NoteStore 
-
+from evernote.edam.notestore import NoteStore # Filter会用到
 #file preparing
 fhand1=open('booktxg.txt','w') 
 txg1=[]
 fhand2=open('notetxg.txt','w') 
 txg2=[]
 
-givennotebooktitle=('[enter the name of the notebooks you need]')
-
+givennotebooktitle=('[enter the name of the notebooks you need]') #这里源码不要忘记改，支持中文笔记本名
 #save all notebooks' guid 
 for notebook in note_store.listNotebooks():
     notebookName = notebook.name
